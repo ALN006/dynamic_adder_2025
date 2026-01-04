1207fdbfedb598e9ec0dadce035b564c6678980b
Aditya LN <adityaln80@gmail.com>
2026-01-04 14:49:04 -0800

    counter fully described and simulated in tb.v

--- adder.v
+++ adder.v
@@ -1,15 +1,15 @@
 module adder_32( //the dynamic adder design 
-    input F, cin, adder_clk,
-    input [31:0] a, b,
-    output cout,
+    input F, Cin, request,
+    input [31:0] A, B,
+    output Cout,
     output [31:0] sum);
 
-    wire [31:0] p, temp_sum; // temp_sum holds sum till ready signal R is 1
+    wire [31:0] P, temp_sum; // temp_sum holds sum till ready signal R is 1
     wire R;
 
-    bitslices_32 add_logic (.a(a), .b(b), .cin(cin), .cout(cout), .sum(temp_sum), .p(p));
+    bitslices_32 add_logic (.a(A), .b(B), .cin(Cin), .cout(Cout), .sum(temp_sum), .p(P));
 
-    timer timing_logic (.clk(adder_clk, ).middle_p(p[17:14]), .F(F), .R(R));
+    timer timing_logic (.clk(adder_clk),.middle_p(p[17:14]), .F(F), .R(R));
 
     buffer_32 output_buffer (.signal(temp_sum), .enable(R), .out(sum));
 
@@ -59,9 +59,9 @@ module buffer_32( //32bit tri-state buffer
 endmodule
 
 
-module timer( //timing logic. limitations -> 1. clk has to be a factor of system clk and greater than three gate delays 2. has the area of approximately 4 bit slices
-    input clk, F, // F is the "First" or start signal
-    input [3:0] middle_p,
+module timer( 
+    input clk, F, // F is the "First" or start signal, it is high for the first clock cycle
+    input [5:0] analyzed_p,
     output R);
 
     wire [3:0] full_time;
@@ -71,19 +71,17 @@ module timer( //timing logic. limitations -> 1. clk has to be a factor of system
 
 endmodule
 
-module counter_4(
-    input clk,
-    input reset,      // Synchronous active-high reset
+module counter_3(     // Asynchronous up counter
+    input reset,      // Asynchronous active-high reset
     output [2:0] q);
 
-    reg S3, S2, S1, S0, S3b, S2b, S1b, S0b;
+    wire S2, S1, S0;
 
-    ff i3 (clk,&{S3^&{S2,S1,S0},~reset},S3,S3b);
-    ff i2 (clk,&{S2^&{S1,S0},~reset},S2,S2b);
-    ff i1 (clk,&{S1^S0,~reset},S1,S1b);
-    ff i0 (clk,&{~S0,~reset},S0,S0b);
+    assign S2 = &{S2^&{S1,S0},~reset};
+    assign S1 = &{S1^&{S0,1'b1},~reset};
+    assign S0 = &{^{1'b1,&{S0,S0}},~reset};
 
-    assign q = {S3,S2,S1,S0};
+    assign q = {S2,S1,S0};
 
 endmodule
 
@@ -95,4 +93,26 @@ module ff (
     always @(posedge clk) Q <= D;
     assign Qb = ~Q; 
 
+endmodule
+
+module timer_3(
+    input F, // the "first" signal
+    output c0,c1,c2
+);
+    parameter and_d = 2, xor_d = 3; 
+    wire xc0, xc1, xc2;
+    wire xc0o, xc1o, xc2o;
+    wire ac0, ac1, ac2;
+    assign xc0o = 1'b1;
+    and #(and_d) a001 (xc1o, 1'b1, c0);
+    and #(and_d) a002 (xc2o, c0, c1);
+    xor #(xor_d) x0 (xc0, ac0, xc0o);
+    xor #(xor_d) x1 (xc1, ac1, xc1o);
+    xor #(xor_d) x2 (xc2, ac2, xc2o);
+    and #(and_d) a00 (c0, xc0, ~F);
+    and #(and_d) a01 (c1, xc1, ~F);
+    and #(and_d) a02 (c2, xc2, ~F);
+    and #(and_d) a0 (ac0, c0, c0);
+    and #(and_d) a1 (ac1, c1, c1);
+    and #(and_d) a2 (ac2, c2, c2);
 endmodule
--- tb.v
+++ tb.v
@@ -1,18 +1,45 @@
 module top_module ();
 	reg clk=0;
-	always #5 clk = ~clk;  // Create clock with period=10
+	always #3 clk = ~clk;  // Create clock with period=6
 	initial `probe_start;   // Start the timing diagram
 
 	`probe(clk);        // Probe signal "clk"
 
 	// A testbench
-	reg in=0;
+	reg in=1;
 	initial begin
-		#10 in <= 1;
-		#10 in <= 0;
-		#20 in <= 1;
-		#20 in <= 0;
+		#8 in <= 0;
 		$display ("Hello world! The current time is (%0d ps)", $time);
-		#50 $finish;            // Quit the simulation
+		#100 $finish;            // Quit the simulation
 	end
+
+    timer_3 inst1 ( .F(in));   // Sub-modules work too.
+
+endmodule
+
+module timer_3(
+    input F, // the "first" signal
+    output c0,c1,c2
+);
+    parameter and_d = 1, xor_d = 1; 
+    wire xc0, xc1, xc2;
+    wire xc0o, xc1o, xc2o;
+    wire ac0, ac1, ac2;
+    assign xc0o = 1'b1;
+    and #(and_d) a001 (xc1o, 1'b1, c0);
+    and #(and_d) a002 (xc2o, c0, c1);
+    xor #(xor_d) x0 (xc0, ac0, xc0o);
+    xor #(xor_d) x1 (xc1, ac1, xc1o);
+    xor #(xor_d) x2 (xc2, ac2, xc2o);
+    and #(and_d) a00 (c0, xc0, ~F);
+    and #(and_d) a01 (c1, xc1, ~F);
+    and #(and_d) a02 (c2, xc2, ~F);
+    and #(and_d) a0 (ac0, c0, c0);
+    and #(and_d) a1 (ac1, c1, c1);
+    and #(and_d) a2 (ac2, c2, c2);
+    
+    `probe(F);
+    `probe(c0);
+    `probe(c1);
+    `probe(c2);
 endmodule

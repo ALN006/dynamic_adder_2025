5a73976d1d75f3a989e51d4ac7043903088651bc
Aditya LN <adityaln80@gmail.com>
2026-01-04 19:37:12 -0800

    bitslices_tb.v works as expected

--- bitslices_tb.v
+++ bitslices_tb.v
@@ -3,15 +3,14 @@ module top_module ();
 	initial `probe_start;   // Start the timing diagram
 
 	// A testbench
-	reg a = 0, b = 0, Cin = 0;
+    reg [31:0] A = 32'hAAAAAAAA, B = 32'h55555555;
+    reg Cin = 0;
 	initial begin
-		
-        #8 a <= 0 ; b <= 1 ; Cin <= 1;
 		$display ("Hello world! The current time is (%0d ps)", $time);
 		#100 $finish;            // Quit the simulation
 	end
 
-    FA inst1 ( .a(a), .b(b), .Cin(Cin));   
+    bitslices_32 inst1 ( .A(A), .B(B), .Cin(Cin));   
 
 endmodule
 
@@ -38,8 +37,22 @@ module bitslices_32(  //instantiates 32 bit slices which are connected ripple ca
     `probe(A);
     `probe(B);
     `probe(Cin);
-    `probe(Cout);
+    `probe(carry);
     `probe(P);
     `probe(sum);
+
+endmodule
+
+module FA( // dynamic adder bitslice
+    input a,b, Cin,
+    output Cout, P, s);
+    parameter nand_d = 1, xor_d = 1; 
+
+    wire nab, nPCin;
+    xor #(xor_d) x0 (P, a, b);
+    xor #(xor_d) x1 (s, P, Cin);
+    nand #(nand_d) n1 (nab, a, b);
+    nand #(nand_d) n2 (nPCin, P, Cin);
+    nand #(nand_d) n3 (Cout, nab, nPCin);
 
 endmodule

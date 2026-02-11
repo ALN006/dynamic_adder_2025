1323f34eb497a602ac397ddfb1b68ad5ab4535bf
Aditya LN <adityaln80@gmail.com>
2026-02-10 20:29:43 -0600

    DRCA simulation tentatively complete the current implementation essentially uses the clock signal to control the output buffer

--- part_sims/DRCA/DRCA.v
+++ part_sims/DRCA/DRCA.v
@@ -4,13 +4,13 @@
 // `include "buffer.v"
 
 //this is a dumb RCA, one clock cycle operation
-module DRCA #(parameter N = 8) (clk, A, B, Cin, Cout, P, S);
+module DRCA #(parameter N = 8) (enable, clk, A, B, Cin, Cout, P, S);
 
     //N is the bitwidth of the DRCA
 
     // I/O declaration
     input [N-1:0] A, B;
-    input clk, Cin;
+    input clk, enable, Cin;
     output Cout;
     output [N-1: 0] P, S;
 
@@ -19,6 +19,6 @@ module DRCA #(parameter N = 8) (clk, A, B, Cin, Cout, P, S);
 
     //logic: buffer releases sum from an RCA after worst case delay (length of clock cycle)
     RCA #(.N(N)) sum_logic (.A(A), .B(B), .Cin(Cin), .Cout(Cout), .P(P), .S(RCA_sum));
-    buffer #(.N(N)) control_logic (.in(RCA_sum), .out(S), .control(clk));
+    buffer #(.N(N)) control_logic (.in(RCA_sum), .out(S), .control(clk & enable));
 
 endmodule
---
+++ part_sims/DRCA/random_DRCA_tb.v
@@ -0,0 +1,60 @@
+`timescale 1ns/1ns
+
+//imports
+`include "DRCA.v"
+`include "RCA.v"
+`include "FA.v"
+`include "buffer.v"
+
+module random_DRCA_tb;
+
+    //setting parameters
+    parameter N = 16;
+
+    // I/O declaration
+    reg [N-1:0] A, B;
+    reg clk = 0, Cin;
+    wire Cout;
+    wire [N-1:0] P, S;
+    always #(N + 1) clk = ~clk; // setup clock signal of period 2N
+
+    //testing variables
+    reg [N:0] expected_sum;
+    integer run_time = 0;
+    integer seedcd = 42; // for reproducibility of random input cases
+
+    //instantiating the design under test
+    DRCA #(.N(N)) dut (.clk(clk), .enable(1'b1), .A(A), .B(B), .Cin(Cin), .Cout(Cout), .P(P), .S(S));
+
+    //measuring runtime
+    always begin
+        #1;
+        if ({Cout, S} !== expected_sum) begin
+            run_time += 1;
+        end
+    end
+
+    //test case generation and output verification
+    initial begin 
+
+        $dumpfile("random_DRCA_tb.vcd");
+        $dumpvars(0, random_DRCA_tb);
+
+        repeat (10000) begin
+            A = $random(seed); B = $random(seed); Cin = $random(seed); //truncation of $random is intended
+            expected_sum  = A + B + Cin;
+            #(2*N + 2)
+            if ({Cout, S} !== expected_sum) begin
+                $display("ERROR: A=%h B=%h Cin=%b | Got=%h Expected=%h",
+                         A, B, Cin, {Cout, S}, expected_sum);
+                //$finish;
+            end
+        end
+
+        $display("Simulation done, runtime was %0d gate delays", run_time);
+        $finish;            //quit simulation
+
+    end
+
+
+endmodule

d9d6893397267854b9614da67ea5b1629b15b9f9
Pranav Kulkarni <pranavkulkarni2104@gmail.com>
2026-02-12 23:04:17 -0600

    fixing errors

--- iverilog_examples/up_counter/up_counter.v
+++ iverilog_examples/up_counter/up_counter.v
@@ -1,10 +1,10 @@
-module up_counter(clk, state, R):
+module up_counter(clk, state, R);
     input clk;
     reg [3:0] state;
     output R;
 
     always @(posedge clk):
-        state <= state + 8`d1;
+        state <= state + 8'd1;
         R = state[3] & state[2] & state[1] & state[0];
     end
 endmodule

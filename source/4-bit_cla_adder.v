136097d6fbda9536fbfddd4e3784a4fa2b4efbe5
andrewjiang42 <andrewjiang172@gmail.com>
2026-02-22 23:08:53 -0600

    Update 4-bit_cla_adder.v

--- source/4-bit_cla_adder.v
+++ source/4-bit_cla_adder.v
@@ -22,16 +22,16 @@ module carry_lookahead_adder_4_bit(
     assign #1 P[2] = ~(A[2] | B[2]);
     assign #1 P[3] = ~(A[3] | B[3]);
 
-    assign #1 C[0] = ~(~C_in);
-    assign #1 C[1] = ~(P[0] | (G[0] & ~C[0]));
-    assign #1 C[2] = ~(P[1] | (P[0] & G[1]) | (G[0] & G[1] & ~C[0]));
-    assign #1 C[3] = ~(P[2] | (P[1] & G[2]) | (P[0] & G[1] & G[2]) | (G[0] & G[1] & G[2] & ~C[0]));
-    assign #1 C[4] = ~(P[3] | (P[2] & G[3]) | (P[1] & G[2] & G[3]) | (P[0] & G[1] & G[2] & G[3]) | (G[0] & G[1] & G[2] & G[3] & ~C[0]));
-    assign #1 C_out = C[4];
+    assign #2 C[0] = ~(~C_in);
+    assign #2 C[1] = ~(P[0] | (G[0] & ~C_in));
+    assign #2 C[2] = ~(P[1] | (P[0] & G[1]) | (G[0] & G[1] & ~C_in));
+    assign #2 C[3] = ~(P[2] | (P[1] & G[2]) | (P[0] & G[1] & G[2]) | (G[0] & G[1] & G[2] & ~C_in));
+    assign #2 C[4] = ~(P[3] | (P[2] & G[3]) | (P[1] & G[2] & G[3]) | (P[0] & G[1] & G[2] & G[3]) | (G[0] & G[1] & G[2] & G[3] & ~C_in));
+    assign C_out = C[4];
 
-    assign #1 S[0] = C[0] ^ (G[0] & ~P[0]);
-    assign #1 S[1] = C[1] ^ (G[1] & ~P[1]);
-    assign #1 S[2] = C[2] ^ (G[2] & ~P[2]);
-    assign #1 S[3] = C[3] ^ (G[3] & ~P[3]);
+    assign #2 S[0] = C_in ^ (G[0] & ~P[0]);
+    assign #2 S[1] = C[1] ^ (G[1] & ~P[1]);
+    assign #2 S[2] = C[2] ^ (G[2] & ~P[2]);
+    assign #2 S[3] = C[3] ^ (G[3] & ~P[3]);
 
 endmodule

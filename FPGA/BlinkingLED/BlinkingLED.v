7f11a47338f13d8dbdb52ca2273e640fcfb562e7
andrewjiang42 <andrewjiang172@gmail.com>
2026-03-17 16:44:48 -0500

    Create BlinkingLED.v

---
+++ FPGA/BlinkingLED/BlinkingLED.v
@@ -0,0 +1,17 @@
+module BlinkingLED (
+	input clk,
+	output reg[0:0] led
+);
+
+reg [24:0] counter;
+
+always @(posedge clk) begin
+	if (counter == 25'd25000000) begin
+		led <= ~led;
+		counter <= 0;
+	end else begin
+		counter <= counter + 1;
+	end
+end
+
+endmodule

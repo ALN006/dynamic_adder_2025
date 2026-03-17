b071e608ee19834cb580410d4bbecbf97d65cee3
andrewjiang42 <andrewjiang172@gmail.com>
2026-03-17 16:46:48 -0500

    Update BlinkingLED.v

--- FPGA/BlinkingLED/BlinkingLED.v
+++ FPGA/BlinkingLED/BlinkingLED.v
@@ -6,7 +6,7 @@ module BlinkingLED (
 reg [24:0] counter;
 
 always @(posedge clk) begin
-	if (counter == 25'd25000000) begin
+	if (counter == 25'd25000000) begin // 50 MHz clock frequency, turn on/off every 25 million cycles to blink every second
 		led <= ~led;
 		counter <= 0;
 	end else begin

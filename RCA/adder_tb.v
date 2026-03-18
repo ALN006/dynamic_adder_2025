faec6bc1177c8e5e104ab1701471a179bed12805
Aditya LN <adityaln80@gmail.com>
2026-03-17 22:45:16 -0700

    the one test bench to rule them all is more or less done

--- RCA/adder_tb.v
+++ RCA/adder_tb.v
@@ -1,15 +1,41 @@
 // this is a test_bench for adders
-//TODO: allow for testing of multiple bitwidths in the same run
+//TODO: core functionality appears complete, test it
 
 `timescale 1ns/1ns 
 
 `include "RCA.v"
 `include "FA.v"
 
-module adder_tb;
+module adder_tb #(parameter dump, tests, start, stop, step, seed, NAND_D, XOR_D);
 
-    // declaring parameters, values are set with makefile
-    parameter N, NAND_D, XOR_D;
+    //csv
+    integer file;
+    initial begin
+        file = $fopen("results.csv", "w");
+        if (file == 0) begin
+            $display("ERROR: Could not open file.");
+            $finish;
+        end
+
+        $fwrite(file, "N,A,B,Cin,Output,Expected,Latency\n");
+        #(tests*100*((stop-start)/step));
+        $fclose(file);
+        $finish;
+    end
+
+    //instantiating adders under test
+    genvar i;
+    generate
+        for ( i = start; i < stop; i = i + step) begin : adder
+            single_test #(.dump(dump), .tests(tests), .N(i), .seed(seed), .NAND_D(NAND_D), .XOR_D(XOR_D)) test_inst (.file(file));
+        end
+    endgenerate
+
+endmodule
+
+
+
+module single_test #(parameter dump, tests, N, seed, NAND_D, XOR_D) (input integer file);
 
     // I/O declaration 
     reg [N-1:0] A, B;
@@ -17,10 +43,50 @@ module adder_tb;
     wire Cout;
     wire [N-1:0] P, S;
 
+
     //adder selection
     `ifdef RCA
         RCA #(.N(N), .NAND_D(NAND_D), .XOR_D(XOR_D)) adder (.A(A), .B(B), .Cin(Cin), .Cout(Cout), .P(P), .S(S));
         initial $display("RCA instantiated as design under test");
     `endif
+
+
+    //testing
+    reg  [N:0] expected_sum;
+    integer latency;
+    integer s;
+
+    initial begin
+
+        //waveform  
+        if (dump == 1)  begin
+            $display("Waveform dumping enabled.");
+            $dumpfile("adder.vcd"); 
+            $dumpvars(0, single_test);    
+        end else begin
+            $display("Note: Waveform dumping disabled.");
+        end
+
+        //stimulus   
+        repeat (tests) begin
+            s = seed;
+            latency = 0;
+            A = $random(s); B = $random(s); Cin = $random(s);
+            expected_sum = A + B + Cin;   
+            #1;
+
+            //measure runtime and wait for execution
+            while (({Cout, S} !== expected_sum) && (latency < 100)) begin
+                latency += 1;
+                #1;
+            end
+
+            if (latency == 100) begin
+                $display("ERROR: N, A, B, Cin, {Cout, S}, expected_sum, latency = %0d,%0d,%0d,%0d,%0d,%0d,%0d\n", N, A, B, Cin, {Cout, S}, expected_sum, latency);
+            end 
+            // Write to CSV
+            $fwrite(file, "%0d,%0d,%0d,%0d,%0d,%0d,%0d\n", N, A, B, Cin, {Cout, S}, expected_sum, latency);
+        end
+    end
 
 endmodule

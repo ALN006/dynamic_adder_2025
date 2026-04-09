5438c09e854207854f17c548be61baabc2eac42f
Pranav Kulkarni <pranavkulkarni2104@gmail.com>
2026-04-08 20:23:15 -0500

    adder2.v stopwatch module updated

--- new_adder/adder2.v
+++ new_adder/adder2.v
@@ -10,78 +10,82 @@ module CRCA #(parameter N = 8, NAND_D = 1, XOR_D = 1) (S, P, Cout, A, B, Cin);
 
     RCA #(.N(N), .NAND_D(NAND_D), .XOR_D(XOR_D)) adder (.A(A), .B(B), .Cin(Cin), .Cout(RCA_out[N]), .P(P), .S(RCA_out[N-1:0]));
 
+
 endmodule
 
 module buffer #(parameter N = 9) (enable, in, out);
-    input [N-1:0] in,
-    input enable,
-    output [N-1:0] out
+    input [N-1:0] in;
+    input enable;
+    output [N-1:0] out;
 
     assign out = enable ? in : {N{1'bz}};
 endmodule
 
-module RCA #(parameter N = 8, NAND_D = 1, XOR_D = 1) (  //instantiates 16 bit slices which are connected ripple carry style
-    input [N-1:0] A, B,
-    input Cin,
-    output Cout,
-    output [N-1:0] P, sum);
-
-    wire [$bits(sum) - 1:0]carry;
-
-    FA #(.nand_d(NAND_D), .xor_d(XOR_D)) instance1 (A[0],B[0],Cin,carry[0],P[0],sum[0]);
-
-    genvar i;
-    generate
-        for ( i = 1; i < $bits(sum); i = i + 1) begin : adder
-            FA #(.nand_d(NAND_D), .xor_d(XOR_D)) u0 (A[i],B[i],carry[i-1],carry[i],P[i],sum[i]);
-        end
-    endgenerate
-
-    assign Cout = carry[$bits(sum) - 1];
-
+module timing_circuit #(parameter N = 8, AND_D = 1, XOR_D = 1, NAND_D = 1) (
+    input [N-1:0] P, sum,
+    input F,
+    input request,
+    output [N-1:0] sum_out
+);
+    integer stopwatch_len = $clog2(2*N);
+    wire [stopwatch_len:0] stopwatch_out;
+    stopwatch #(stopwatch_len, AND_D, XOR_D) watch (.F(F), .out(stopwatch_out));
+    wire fourth, half, three_fourth, full; //how do you parameterize this??
+    wire s2, s1, s0;
+    nand #(NAND_D) nand0 (s0, P[N / 4], P[N / 4 - 1]);
+    nand #(NAND_D) nand1 (s1, P[N / 2], P[N / 2 - 1]);
+    nand #(NAND_D) nand2 (s2, P[3 * N / 4], P[(3 * N / 4) - 1]);
+    wire ready;
+    mux_3_8 mux(
+        .option({fourth, half, half, three_fourth, half, half, three_fourth, full}),
+        .select({s2, s1, s0}),
+        .F(F),
+        .out(ready)
+    );
+    wire rlease;
+    and #(and_d) (rlease, ready, request);
+    buffer #(N) buf(.enable(rlease), .in(sum), .out(sum_out));
 endmodule
 
-
-module FA #(parameter nand_d = 1, xor_d = 1) ( // dynamic adder bitslice
-    input a,b, Cin,
-    output Cout, P, s);
-
-    wire nab, naCin, nbCin, temp;
-    xor #(xor_d) x0 (P, a, b);
-    xor #(xor_d) x1 (s, P, Cin);
-    nand #(nand_d) n0 (nbCin, b, Cin);
-    nand #(nand_d) n1 (nab, a, b);
-    nand #(nand_d) n2 (naCin, a, Cin);
-    nand #(nand_d) n3 (Cout, nab, nbCin, naCin);
-
-endmodule
-
-module stopwatch #(parameter N = 8, AND_D = 1, XOR_D = 1) (
+//module timing_circuit #(parameter N = 8, NAND_D = 1, AND_D = 1, OR_D = 1) 
+module stopwatch #(parameter N = 3, AND_D = 1, XOR_D = 1) (
     input F, 
     output [N - 1: 0] out
 );
     wire [N-1:0] and1_out;
     wire [N-1:0] xor_out; 
-    wire [N-2:0] and2_out;
+    wire [N-1:0] and2_out;
 
-    and #(AND_D) delay_and (and1_out[0], out[0], out[0]);
-    xor #(XOR_D) main_x (xor_out[0], and1_out[0], 1'b1);
-    and #(AND_D) f_and (out[0], xor_out[0], ~F);
-
-    and #(AND_D) prev_and (and2_out[0], out[0], 1'b1);
-    and #(AND_D) delay_and2 (and1_out[1], out[1], out[1]);
-    xor #(XOR_D) main_x2 (xor_out[1], and1_out[1], and2_out[0]);
-    and #(AND_D) f_and2 (out[1], xor_out[1], ~F);
-
-    integer i;
+    genvar i;
     generate
-        for(i = 2; i < N; i = i + 1) begin
-            and #(AND_D) prev_and_N (and2_out[i - 1], out[i - 1], and2_out[i - 2]);
+        for (i = 0; i < N; i ++) begin : delay_and
             and #(AND_D) delay_and_N (and1_out[i], out[i], out[i]);
-            xor #(XOR_D) main_x_N (xor_out[i], and1_out[i], and2_out[i - 1]);
             and #(AND_D) f_and_N (out[i], xor_out[i], ~F);
-        end
+            assign and2_out[i] = &{out[i:0], 1'b1};
+            xor #(XOR_D) main_x_N (xor_out[i], and1_out[i], and2_out[i]);
+        end 
     endgenerate
+
+    // first bit
+    // and #(AND_D) delay_and (and1_out[0], out[0], out[0]);
+    // xor #(XOR_D) main_x (xor_out[0], and1_out[0], 1'b1);
+    // and #(AND_D) f_and (out[0], xor_out[0], ~F);
+
+    // //second bit
+    // and #(AND_D) prev_and (and2_out[0], out[0], 1'b1);
+    // and #(AND_D) delay_and2 (and1_out[1], out[1], out[1]);
+    // xor #(XOR_D) main_x2 (xor_out[1], and1_out[1], and2_out[0]);
+    // and #(AND_D) f_and2 (out[1], xor_out[1], ~F);
+
+    // integer i;
+    // generate
+    //     for(i = 2; i < N; i = i + 1) begin
+    //         and #(AND_D) prev_and_N (and2_out[i - 1], out[i - 1], and2_out[i - 2]);
+    //         and #(AND_D) delay_and_N (and1_out[i], out[i], out[i]);
+    //         xor #(XOR_D) main_x_N (xor_out[i], and1_out[i], and2_out[i - 1]);
+    //         and #(AND_D) f_and_N (out[i], xor_out[i], ~F);
+    //     end
+    // endgenerate
 endmodule
 
 module mux_3_8( // 3*8 mux that captures any selected signal in 3 gate delays so long as it was high for atleast 2 gate delays
